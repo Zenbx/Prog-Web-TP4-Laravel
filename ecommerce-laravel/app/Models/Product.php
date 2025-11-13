9bc6d39da48899c67befd6793d0a67944a3db578
Jeff Belekotan <jeffbelekotan@gmail.com>
2025-11-13 11:49:03 +0100

    Attributs Produits

--- ecommerce-laravel/app/Models/Product.php
+++ ecommerce-laravel/app/Models/Product.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Product extends Model {
     use HasFactory;
 
-    protected $fillable = ['name','description','price','stock','sku'];
+    protected $fillable = ['name','description','category','brand','price','stock','sku'];
 }
